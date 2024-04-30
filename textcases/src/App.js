1bc6957c429eaed63f19457e4cf11476e99fa62b
Prathamesh Surywanshi <psurywanshi620@gmail.com>
2024-05-01 02:19:40 +0530

    Footer Added

--- textcases/src/App.js
+++ textcases/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <>
     <Navbar/>
     <Home/>
-    
+    <Footer/>
     </>
   );
 }
