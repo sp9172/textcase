9259fa300913d63c3ca19a7d2f53379bffedabff
Prathamesh Surywanshi <psurywanshi620@gmail.com>
2024-05-01 02:21:05 +0530

    Update Footer.js

--- textcases/src/Components/Footer.js
+++ textcases/src/Components/Footer.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 export default function Footer(){
+    return(
 
-    <><div className="container">
+    <>
+    <div className="container">
     <footer className="py-3 my-4">
       <ul className="nav justify-content-center border-bottom pb-3 mb-3">
         <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Home</a></li>
@@ -15,4 +17,5 @@ export default function Footer(){
     </footer>
   </div>
   </>
+    )
 }
